247cb3495a90e6234a313767046ad7abc9bf6263
ditog <ditog@DESKTOP-L8AIV4A>
2020-03-17 22:55:26 +0900

    Preparing AOP Process_2020/03/17

--- seowonhm.sql
+++ seowonhm.sql
@@ -33,6 +33,28 @@ create table seowonhm.tsn_reply (
  CONSTRAINT fk_board FOREIGN KEY(bno) references seowonhm.tsn_board(bno)
  );
 
+-- user Table
+create table seowonhm.tsn_user(
+ uid varchar(50) NOT NULL,
+ upw varchar(50) NOT NULL,
+ uname varchar(100) NOT NULL,
+ upoint int NOT NULL DEFAULT 0,
+ primary key(uid)
+);
+
+-- message Table
+create table seowonhm.tsn_message(
+ mid int NOT NULL AUTO_INCREMENT,
+ targetid varchar(50) NOT NULL,
+ sender varchar(50) NOT NULL,
+ message text NOT NULL,
+ opendate timestamp,
+ senddate timestamp NOT NULL DEFAULT NOW(),
+ primary key(mid),
+ constraint fk_usertarget foreign key(targetid) references seowonhm.tsn_user(uid),
+ constraint fk_usersender foreign key(targetid) references seowonhm.tsn_user(uid)
+);
+
 TRUNCATE TABLE seowonhm.tsn_reply;
 
 ALTER TABLE seowonhm.tsn_reply DROP FOREIGN KEY fk_board;
